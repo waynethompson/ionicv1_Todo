3606698de8405b7b2ac4af29f6525fd1be7cd07e
Wayne Thompson <wayne_thompson@hotmail.com>
2017-08-12 23:02:40 +1000

    created seperate controller file

---
+++ todo_ionic/www/js/todo.controller.js
@@ -0,0 +1,33 @@
+angular.module('todo')
+
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  // No need for testing data anymore
+  $scope.tasks = [];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
+})
